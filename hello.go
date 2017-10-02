a48cd3e30bdffa009f87cbecdcd439bc45f1b799
Israel Garcia <igarcia@itexico.net>
2017-10-02 16:42:11 -0500

    User Types/Structs

--- hello.go
+++ hello.go
@@ -2,6 +2,19 @@ package main
 
 import  "fmt"
 
+
+
+//This is the same type as string, but with our own name
+type Salutation string
+
+//Or we can define our Structure/Object, since Go does not have Classes as everyone else
+//Also, this struct is public visible since it starts on Capital instead lowercase
+type SalutationStruct struct{
+	name string
+	greeting string
+}
+
+
 func main()  {
 	//Go Declares types after the variable
 	var message string
@@ -34,4 +47,24 @@ func main()  {
 
 	fmt.Println(message,*greeting) //prints the new value
 
+	var phrase Salutation = "Hello World Salutation"
+	fmt.Println(phrase)
+
+	//In here we can initialize our variable sending the parameters while initializing
+	var phraseStruct = SalutationStruct{"Pekosog","Hello There"}
+
+	//There are no concepts of getters or setters, we can access vaclues as follow
+	fmt.Println(phraseStruct.name)
+	fmt.Println(phraseStruct.greeting)
+
+	//We can also send the parameters in any order BUT we need to define the variable name
+	phraseStruct = SalutationStruct{greeting:"Hello there",name:"PekosoG"}
+
+	fmt.Println(phraseStruct.name)
+	fmt.Println(phraseStruct.greeting)
+
+	//Or we can initialize in blank and set the values afterwards
+	phraseStruct = SalutationStruct{}
+	phraseStruct.name="PekosoG"
+	phraseStruct.greeting="Hello There"
 }
