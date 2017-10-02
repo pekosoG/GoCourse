a86b01526a5837ee5e6adcccaa0f90e66fc22671
Israel Garcia <igarcia@itexico.net>
2017-10-02 14:47:29 -0500

    Basic and Multi-Declaration

--- hello.go
+++ hello.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Print("Hello World!")
+	//Go Declares types after the variable
+	var message string
+
+	message = "Hello world"
+
+	fmt.Println(message)
+
+	//Multiple variables at the same time and values as well
+	var a,b,c int = 3,4,5
+
+	fmt.Println(a,b,c)
+
+	var messageStr = "Hello Go world"
+
+	fmt.Println(messageStr)
+
+	messagePoints := "Hello Go World DoublePoints"
+	x,y,z:= 2, false, "zZz"
+
+	fmt.Println(messagePoints,x,y,z)
+
 }
