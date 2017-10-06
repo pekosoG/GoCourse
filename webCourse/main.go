cb21d34f54b2403611ee72b13c6bf5e6c1505984
Israel Garcia <igarcia@itexico.net>
2017-10-06 11:16:26 -0500

    Custom ServeHttp handler

--- webCourse/main.go
+++ webCourse/main.go
@@ -14,11 +14,21 @@ func testRoute(rw http.ResponseWriter, req *http.Request){
 }
 
 func helloUser(rw http.ResponseWriter, req *http.Request){
-	fmt.Fprintf(rw,"<h1>Some User route</h1>")
+
+}
+
+type message struct{
+	text string
+}
+
+/**
+	This is a Custom handler that we can define with a Signature that in this case is message
+ */
+func(msg message) ServeHTTP(resp http.ResponseWriter, req *http.Request){
+	fmt.Fprintf(resp,msg.text)
 }
 
 func main(){
-
 
 	routerMux:= http.NewServeMux();
 
@@ -27,6 +37,13 @@ func main(){
 	routerMux.HandleFunc("/test",testRoute)
 
 	routerMux.HandleFunc("/user",helloUser)
+
+	someMessage:=message{text:"Hello There"}
+
+	/*
+		And since we have the default handler defined, our RouterMux recognize it and use it
+	 */
+	routerMux.Handle("/hello",someMessage)
 
 	http.ListenAndServe(":8080",routerMux)
 }
