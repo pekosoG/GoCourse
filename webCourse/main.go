ad1c78f7dbf0449f48c8bda2ff136134024b59b8
Israel Garcia <igarcia@itexico.net>
2017-10-06 10:48:43 -0500

    functions on routes

--- webCourse/main.go
+++ webCourse/main.go
@@ -5,11 +5,28 @@ import (
 	"fmt"
 )
 
+func helloWorld(rw http.ResponseWriter, req *http.Request){
+	fmt.Fprintf(rw,"<h1>Hello World GoLan</h1>")
+}
+
+func testRoute(rw http.ResponseWriter, req *http.Request){
+	fmt.Fprintf(rw,"<h1>Testing routing!</h1>")
+}
+
+func helloUser(rw http.ResponseWriter, req *http.Request){
+	fmt.Fprintf(rw,"<h1>Some User route</h1>")
+}
+
 func main(){
 
-	http.HandleFunc("/",func(rw http.ResponseWriter, req *http.Request){
-		fmt.Fprintf(rw,"<h1>Hello World GoLan</h1>")
-	})
 
-	http.ListenAndServe(":8080",nil)
+	routerMux:= http.NewServeMux();
+
+	routerMux.HandleFunc("/",helloWorld)
+
+	routerMux.HandleFunc("/test",testRoute)
+
+	routerMux.HandleFunc("/user",helloUser)
+
+	http.ListenAndServe(":8080",routerMux)
 }
